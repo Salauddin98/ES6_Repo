b7d991933bcdc1acceecaca585fc35e6b222e90f
Salauddin98 <webdevelopment20236@gmail.com>
2023-03-05 11:41:45 +0600

    added

--- practiceApi/tempAPI/script.js
+++ practiceApi/tempAPI/script.js
@@ -1,6 +1,6 @@
 const key = `9d90b52ffb668011088f92a3fe68380f`
 
-const loadTempData = (city, name) => {
+const loadTempData = (city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
     fetch(url)
         .then(res => res.json())
@@ -14,8 +14,10 @@ const showTempData = (data) => {
     } else {
         const temperature = document.getElementById('temperature');
         const city = document.getElementById('city');
+        const condition = document.getElementById('condition');
         temperature.innerText = data.main.temp
         city.innerText = data.name
+        condition.innerText = data.weather[0].main;
     }
 
 }
