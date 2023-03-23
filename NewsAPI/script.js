ff19ae6401ceb281a7dd85ed3cadaac2abd1b088
Salauddin98 <webdevelopment20236@gmail.com>
2023-03-23 10:27:09 +0600

    add

--- NewsAPI/script.js
+++ NewsAPI/script.js
@@ -1,3 +1,5 @@
+let fetchData = [];
+
 const navbarDataLoad = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
@@ -27,23 +29,27 @@ const showNavbarCategory = (data) => {
 
 //News Show Data---->
 const newsDataLoad = (category_id, category_name) => {
+    // console.log(category_name)
     const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`
     // console.log(category_id,category_name)
 
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            showNewsDataLoad(data, category_name)
+            // console.log(data)
+            fetchData = data.data
+            showNewsDataLoad(data.data, category_name)
         })
 }
 
-const showNewsDataLoad = (id, category_name) => {
-    console.log(id)
+const showNewsDataLoad = (data, category_name) => {
     const showData = document.getElementById('showData');
     const alertTitle = document.getElementById('alertTitle');
-    alertTitle.innerText = `${id.data.length} items found for ${category_name}`
+    const alertCount = document.getElementById('alertCount');
+    alertCount.innerText = `${data.length} items found for `
+    alertTitle.innerText = `${category_name}`
     showData.innerHTML = '';
-    id.data.map(item => {
+    data.map(item => {
         // alertTitle.innerHTML = `items ${item.length}`
         const { author, title, thumbnail_url, image_url, details, total_view, rating, _id } = item;
         // console.log(author.img)
@@ -53,13 +59,13 @@ const showNewsDataLoad = (id, category_name) => {
             <div class="col-md-4">
                 <img class="img-fluid" src="${image_url}" class="img-fluid rounded-start" alt="...">
             </div>
-            <div class="col-md-8">
+            <div class="col-md-8 ">
                 <div class="card-body">
                     <h5 class="card-title">${title}</h5>
                     <p class="card-text">${details.length > 400 ? details.slice(0, 200) + '...' : details}</p>
                    <div class= "d-flex justify-content-around align-items-center">
                    <div class="d-flex gap-4 align-items-center">
-                   <img class="img-fluid" style="width:70px;height:70px;border-radius:50px" src="${author.img}">
+                   <img class="img-fluid rounded-circle" style="width:70px;height:70px;" src="${author.img}">
                    <div >
                    <h6>${author.name ? author.name : 'No Found Name'}</h6>
                    <h6>${author.published_date}</h6>
@@ -104,7 +110,7 @@ const showModalDetails = (data) => {
     data.data.map(item => {
         modalBody.innerHTML += `
        <img class="img-fluid" src="${item.image_url}">
-       <h5 class="card-title mt-4">${item.title}</h5>
+       <h5 class="card-title mt-4 ${item.others_info.is_trending || item.others_info.is_todays_pick ? 'd-block' : 'd-none'}">${item.title ? item.title : ""} <span class="badge text-bg-danger">${item.others_info.is_trending ? "Trending" : ""}</span> <span class="badge text-bg-success">${item.others_info.is_todays_pick ? "Todays Pick" : ""}</span></h5>
        <p>${item.details}</p>
 
        <div class= "d-flex justify-content-around align-items-center">
@@ -112,6 +118,7 @@ const showModalDetails = (data) => {
                    <img class="img-fluid" style="width:70px;height:70px;border-radius:50px" src="${item.author.img}">
                    <div class="ms-3">
                    <h6>${item.author.name ? item.author.name : 'No Found Name'}</h6>
+                   
                    <h6>${item.author.published_date}</h6>
                    </div>
                    </div>
@@ -127,4 +134,45 @@ const showModalDetails = (data) => {
                    </div>
         `
     })
+}
+
+
+// const todayPickShow = () => {
+//     let fetchDates = fetchData.filter(item => {
+//         item.others_info.is_todays_pick === true;
+//     })
+//     showNewsDataLoad(fetchDates)
+// }
+
+const trendingNews = () => {
+    // console.log(fetchData)
+    let trendingNews = fetchData.filter(item => item.others_info.is_trending === true)
+    if (trendingNews.length === 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No Found Trending News!!',
+            // footer: '<a href="">Why do I have this issue?</a>'
+        })
+    } else {
+        // console.log(trendingNews)
+        const alertTitle = document.getElementById('alertTitle').innerText;
+        showNewsDataLoad(trendingNews, alertTitle)
+    }
+
+}
+
+const todayPickShow = () => {
+    const todayPickShow = fetchData.filter(item => item.others_info.is_todays_pick === true);
+    if (todayPickShow.length === 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No Found Todays Pick News!!',
+            // footer: '<a href="">Why do I have this issue?</a>'
+        })
+    } else {
+        const alertTitle = document.getElementById('alertTitle').innerText;
+        showNewsDataLoad(todayPickShow, alertTitle)
+    }
 }
